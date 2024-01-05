ae67194c22a47f8a4a195cf68e832f38fb004765
nazarprydatko <155411757+nazarprydatko@users.noreply.github.com>
2024-01-05 12:04:44 +0200

    Update ComicBook.java

--- ComicBook.java
+++ ComicBook.java
@@ -1,16 +1,5 @@
 package bookshelf.bookstore;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author prida
- */
-
 class ComicBook extends Book {
     private boolean color;
 
