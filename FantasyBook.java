0e8ba4f10f5404941ae8487d1607f699d970822f
nazarprydatko <155411757+nazarprydatko@users.noreply.github.com>
2024-01-05 12:04:26 +0200

    Update FantasyBook.java

--- FantasyBook.java
+++ FantasyBook.java
@@ -1,10 +1,5 @@
 package bookshelf.bookstore;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 import bookshelf.bookstore.Book;
 
 /**
