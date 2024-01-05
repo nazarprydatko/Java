ddd5563d6452427189f0810f3c75fedf8ef1bb0e
nazarprydatko <155411757+nazarprydatko@users.noreply.github.com>
2024-01-05 12:04:56 +0200

    Update BookShelf.java

--- BookShelf.java
+++ BookShelf.java
@@ -1,20 +1,11 @@
 package bookshelf;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 import bookshelf.bookstore.Book;
 
 import java.util.LinkedList;
 import java.util.List;
 
 
-/**
- *
- * @author prida
- */
 class BookShelf {
     private static BookShelf instance;
     private final List<Book> storage;
